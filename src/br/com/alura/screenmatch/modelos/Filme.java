f0e57edfd73d9468ff87c2d6cd75f766b4d1be4c
Willder Canabrava <willcanabrava@gmail.com>
2023-07-12 12:57:13 -0300

    Initial commit

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,47 @@
+package br.com.alura.modelos;
+
+public class Filme {
+
+    String nome;
+    private int anoDeLancamento;
+    private int duracaoEmMinutos;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public double getSomaDasAvaliacoes() {
+        return somaDasAvaliacoes;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void exibeFichaTecnica () {
+        System.out.println("Nome: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duração: " + duracaoEmMinutos);
+    }
+
+    public void avalia (double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia () {
+        return somaDasAvaliacoes/totalDeAvaliacoes;
+    }
+}
+
